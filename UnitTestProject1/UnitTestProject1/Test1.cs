22c7a3d7fcd2b0b3907b6225751238aa76d4cf16
Евсюков Максим (m.evsyukov) <m.evsyukov@2gis.ru>
2017-07-17 14:04:51 +0700

    vse

--- UnitTestProject1/UnitTestProject1/Test1.cs
+++ UnitTestProject1/UnitTestProject1/Test1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Reflection;
 using System.Security.Cryptography;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -13,13 +15,20 @@ namespace UnitTestProject1
         public void TestMethod1()
         {
             IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
             driver.Url = "http://uk-youla-iis/login";
             var login = driver.FindElement(By.Id("username"));
             var password = driver.FindElement(By.Id("password"));
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
             login.SendKeys("local"+"\\"+"a");
             password.SendKeys("a");
-            driver.FindElement(By.ClassName("btn btn-primary")).Click();
-            Assert.AreEqual("a", driver.FindElement(By.ClassName("btn dropdown-toggle")));
+            driver.FindElement(By.ClassName("button-login")).Click();
+            var element = wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.XPath("//html/body/div[1]/div/div/div/ul/li[4]/a/span[1]")));
+
+            var bodyText = driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/ul/li[4]/a/span[1]"));
+            Assert.AreEqual("a",Convert.ToString(bodyText.Text));
+
             driver.Quit();
         }
 
