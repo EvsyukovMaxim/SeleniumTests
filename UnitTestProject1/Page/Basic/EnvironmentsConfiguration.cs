b48e9339189f1fba11fe565755501c024e0ba9b2
Евсюков Максим (m.evsyukov) <m.evsyukov@2gis.ru>
2017-10-10 23:22:48 +0700

    NewEnvironment

--- UnitTestProject1/Page/Basic/EnvironmentsConfiguration.cs
+++ UnitTestProject1/Page/Basic/EnvironmentsConfiguration.cs
@@ -2,6 +2,6 @@
 {
     public class EnvironmentsConfiguration
     {
-        public static string CurrentEnvironmentBaseUrl => "http://youla.dev:8080";
+        public static string CurrentEnvironmentBaseUrl => "http://youla.dev";
     }
 }
